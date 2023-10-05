9d31fffaa2b3cc066bb6144b6c3b5635a71cc4af
Tim Hunter <timh@telnyx.com>
2023-10-06 00:27:45 +0100

    internal code tidy/optimize
    - separate function to process lines into entries
    - separate function to process entries
    - instead of 'print first' array and 'the rest' array, use one array and a
      custom sort routine
    - write warnings/errors to stderr
    - same read/write code for filename & stdin/stderr
    
    ...all in preparation for next step, which is representing multiple wav files
    in one tsv

--- labels/sort_tsv.py
+++ labels/sort_tsv.py
@@ -4,6 +4,7 @@ import shutil
 import re
 import sys
 import argparse
+import contextlib
 
 # Initialize the argument parser
 parser = argparse.ArgumentParser(description='Process and adjust input data.')
@@ -29,15 +30,14 @@ if filename and not test_mode:
     # If we have a .txt extension, move it to .tsv
     if filename.endswith("txt"):
         moveresult = shutil.move(filename, filename[:-3] + "tsv")
-        print(f"Moved {filename} to {moveresult}")
+        sys.stderr.write(f"Moved {filename} to {moveresult}\n")
         filename=filename[:-3] + "tsv"
 
     backup_filename = filename + ".bak"
     shutil.copy(filename, backup_filename)
 
 # Initialize lists to store lines to be sorted and unrecognized lines
-file_start_lines = []
-sort_lines = []
+sort_lines = []         # stores entries of the form (timestamp1, timestamp2, label, line number)
 line_number = 0
 
 # Define regular expressions for matching keywords
@@ -53,23 +53,21 @@ keyword_patterns = [
 # Compile regular expressions
 keyword_regexes = [re.compile(pattern) for pattern in keyword_patterns]
 
-# Function to process lines
-def process_line(line):
-    global line_number
+# Function to sort lines -- put "file start" at the top, always, then sort by
+# timestamp, then sort by label
+# take in tuple (timestamp, label) -- return tuple (startline, timestamp, label)
+def tracksort(entry):
+    startline="B"
+    if re.match(r"file start", entry[2]):
+        startline="A"
+    return (startline, entry[0], entry[1], entry[2])
+
+# Function to place tracklist entries onto sorted_lines or second_files
+def process_entry(parts):
     global adjust_value
-    line_number += 1
-    parts = line.strip().split('\t')
-    if (len(parts) < 3):
-        print(f"Warning: Unrecognized line - less than 3 fields - {line.strip()}")
-        return
 
+    # first, is this a "secondary file" entry?
 
-    if re.match(r"file start", parts[2]):
-        adjust_value = float(parts[0])
-        print(f"FIRST: adj({adjust_value}) {line.strip()}")
-        file_start_lines.append(line)
-        return
-        
     matched_keyword = None
     # Check for keywords in the text portion of the line
     for pattern in keyword_regexes:
@@ -79,58 +77,62 @@ def process_line(line):
             break
 
     if not matched_keyword:
-        print(f"Warning: Unrecognized keywords ({parts[2]}) at line {line_number} ts {parts[0]}")
+        sys.stderr.write(f"Warning: Unrecognized keywords ({parts[2]}) at line {parts[3]} ts {parts[0]}\n")
 
-    try:
-        first_float = float(parts[0])
-        sort_lines.append((first_float, line))
-    except ValueError:
-        print(f"Warning: Unrecognized line - first value not a float - {line.strip()}")
+    if re.match(r"file start", parts[2]):
+        adjust_value = float(parts[0])
+        sys.stderr.write(f"FIRST: adj({adjust_value}) {line.strip()}\n")
 
+    sort_lines.append((parts))
 
-# Process input based on whether a filename is provided or not
-if filename:
-    with open(filename, 'r') as input_file:
-        for line in input_file:
-            process_line(line)
-else:
-    # Read from stdin
-    for line in sys.stdin:
-        process_line(line)
+# Function to turn line strings into tracklist entries and process them with process_entry
+def process_line(line):
+    global line_number
+    line_number += 1
+    parts = line.strip().split('\t')
+    parts.append(line_number)
 
-# Sort the sort_lines by timestamp
-sort_lines.sort(key=lambda x: (x[0], x[1]))
+    # line validation checks
+    if (len(parts) < 3):
+        sys.stderr.write(f"Warning: Unrecognized line - less than 3 fields - {line.strip()}\n")
+        return
+
+    # float check
+    for i in [0,1]:
+        try:
+            partfloat = float(parts[i])
+            parts[i] = partfloat
+        except ValueError:
+            sys.stderr.write(f"Warning: Unrecognized line - parts[{i}]={parts[i]} not a float - {line.strip()}\n")
+            return
+
+    process_entry(parts)
 
 # adjust timestamps if appropriate
-def adjust_line(line):
+def adjust_line(entry):
     global adjust_value
-    parts = line.split('\t')
     for i in range(2):
-        parts[i] = float(parts[i])
-        parts[i] -= adjust_value
-    return f"{parts[0]:.6f}\t{parts[1]:.6f}\t{parts[2]}"
+        entry[i] -= adjust_value
+    return entry
 
+## MAIN ##
+
+# Process input based on whether a filename is provided or not
+with contextlib.ExitStack() as stack:
+    input = stack.enter_context(open(filename, 'r')) if filename else sys.stdin
+    for line in input:
+        process_line(line)
+
+sort_lines.sort(key=tracksort)
 
 if do_adjustment:
-    file_start_lines = list(map(adjust_line, file_start_lines))
-    sort_lines = [(number, adjust_line(line)) for number, line in sort_lines]
+    sort_lines = map(adjust_line, sort_lines)
 
 # Write the sorted lines and file start lines to the appropriate output
-if filename and not test_mode:
-    with open(filename, 'w') as output_file:
-        for line in file_start_lines:
-            output_file.write(line)
-        for _, line in sort_lines:
-            output_file.write(line)
-elif not test_mode:
-    # Write to stdout
-    for line in file_start_lines:
-        sys.stdout.write(line)
-    for _, line in sort_lines:
-        sys.stdout.write(line)
+if not test_mode:
+    with contextlib.ExitStack() as stack:
+        output = stack.enter_context(open(filename, 'w')) if filename else sys.stdout
+        for entry in sort_lines:
+            output.write("{:.6f}\t{:.6f}\t{}\n".format(*entry))
 
-# Remove the backup file if it was created
-#if filename:
-#   shutil.rmtree(backup_filename)
-
-print("Processing complete.")
+sys.stderr.write("Processing complete.\n")
