7044a184c0c8d85a5b675d67dfb61fad7ec663a6
Tim Hunter <timh@telnyx.com>
2023-11-10 23:23:40 +0000

    finish splitexport, but still some TODO
    - convert code to use get_info with 'JSONlist' format
    - TODO: change pyaudacity as discussed [here](https://github.com/asweigart/pyaudacity/issues/7) and update this code to match
    - TODO: further changes to use pyaudacity instead of rougher API calls
    - Change all the setlabel warnings to be debug warnings
    - implement export_by_label() to export tracks by label as wav, convert to wv,
      and apply tags
    - ask if labels should be created when some already exist
    - implement pre-export user sanity check

--- scripts/splitexport.py
+++ scripts/splitexport.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 import sys
 import json
@@ -5,6 +6,7 @@ import re
 import time
 import pprint
 import pyaudacity as pa
+import subprocess
 
 outline = """
     - create a label track if necessary
@@ -16,9 +18,11 @@ outline = """
 """
 
 debug = False                           # debug level
+debug_dump = False                      # dump data structures after or as read
 
 aud_tracks = None                       # list of audacity tracks
 aud_clips = None                        # list of audacity audio clips
+aud_labels = None                       # list of audacity labels
 audacitytest = False                    # audacity debug
 samplerate = None                       # sample rate of the audio track
 
@@ -40,27 +44,21 @@ def audcommand(string):
 
 # initialize audacity data
 def audinit():
-    global aud_tracks, aud_clips, samplerate
+    global aud_tracks, aud_clips, samplerate, aud_labels
 
     # Pull Track info if we don't have them already
     # probably not needed but good as an init test
     if not aud_tracks:
-        jdata = pa.get_info(info_type='Tracks')
-        jdata = (jdata [:jdata.rfind(']')+1])
-        aud_tracks = json.loads(jdata)
+        aud_tracks = pa.get_info(info_type='Tracks', format="JSONlist")
         dprint(aud_tracks)
         # format of response
         # [{dict about track}, ...]
 
     if not aud_clips:
-        jdata = pa.get_info(info_type='Clips')
-        jdata = (jdata [:jdata.rfind(']')+1])
-        aud_clips = json.loads(jdata)
+        aud_clips = pa.get_info(info_type='Clips', format="JSONlist")
 
     if not samplerate:
-        jdata = pa.get_info(info_type='Preferences')
-        jdata = (jdata [:jdata.rfind(']')+1])
-        prefs = json.loads(jdata)
+        prefs = pa.get_info(info_type='Preferences', format="JSONlist")
         for p in prefs:
             if p['id'] == '/SamplingRate/DefaultProjectSampleRate':
                 samplerate = int(p['default'])
@@ -70,13 +68,17 @@ def audinit():
     if not samplerate:
         sys.exit("Could not find samplerate in GetInfo: Type=Preferences")
 
+    if not aud_labels:
+        aud_labels = pa.get_info(info_type='Labels', format="JSONlist")
+
+
 # given appropriate details, set a label on a given track
 def setlabel(LabelTrack,ts,label,endts=None):
-    global samplerate
+    global samplerate, debug_dump, aud_labels
 
     setts = False   # by default, don't set the timestamp when setting label text, but do if we found it at ts=0
 
-    dprint(f"setlabel({LabelTrack}, {ts}, {label}, endts={endts})")
+    dprint(f".setlabel({LabelTrack}, {ts}, {label}, endts={endts})")
     if (LabelTrack is None) or (ts is None) or (label is None):
         sys.exit(f"invalid arguments to setlabel({LabelTrack}, {ts}, {label}, endts={endts})")
 
@@ -86,7 +88,7 @@ def setlabel(LabelTrack,ts,label,endts=None):
     # Adjust the timestamps
     for i in [0,1]:
         # round down ts to 8 digits to avoid python math wierdness
-        dprint(f"ts[{i}] fractions of a second in samples: {(ts[i] - int(ts[i]))*samplerate}")
+        dprint(f".ts[{i}] fractions of a second in samples: {(ts[i] - int(ts[i]))*samplerate}")
         ts[i]=round(ts[i],8)
 
         # check precision of the ts
@@ -97,11 +99,11 @@ def setlabel(LabelTrack,ts,label,endts=None):
             sec = int(ts[i])%60
             samples = int((ts[i] - int(ts[i])) * samplerate)
             if note and (ts[0] != pass_ts):
-                dprint(f"ADDING TO NOTE AS ts0 ({ts[0]}) != ts1/pass_ts ({pass_ts})")
+                dprint(f".ADDING TO NOTE AS ts0 ({ts[0]}) != ts1/pass_ts ({pass_ts})")
                 note += "-"
             if not note or (ts[0] != pass_ts):
                 note += f"({min:02d}:{sec:02d} + {samples} samples)"
-            sys.stderr.write(f"NOTE: adding {note} to label to note correct label placement\n")
+            dprint(f".NOTE: adding {note} to label to note correct label placement\n")
         ts[i] = pass_ts
 
     label += " " + note
@@ -111,29 +113,27 @@ def setlabel(LabelTrack,ts,label,endts=None):
     audcommand("AddLabel:")
 
     # now, stupidly, we need to find the # of the label we just added
-    jdata = pa.get_info(info_type='Labels')
-    jdata = (jdata [:jdata.rfind(']')+1])
-    aud_labels = json.loads(jdata)
+    aud_labels = pa.get_info(info_type='Labels', format="JSONlist")
         # [labeltrack, ...]
         # where each label track is [tracknumber, [[ts1, ts2, label], ...]
     labelnum = 0
     newlabelnum = None
     justblank = []
     for lt in aud_labels:
-        dprint(f"label track {lt[0]}")
+        if debug_dump: print(f"label track {lt[0]}")
         for l in lt[1]:
-            dprint(f"label{labelnum}: {l}")
+            if debug_dump: print(f"label{labelnum}: {l}")
             if l[2] == '' and l[0] == ts[0] and l[1] == ts[1]:
                 # this is our label number
                 newlabelnum = labelnum
                 break
             elif l[2] == '':
-                print(f"Blank label# {labelnum} start={l[0]} end={l[1]}")
+                dprint(f".Blank label# {labelnum} start={l[0]} end={l[1]}")
                 justblank.append(labelnum)
             labelnum += 1
     if (newlabelnum is None) and not audacitytest:
-        sys.stderr.write(f"Could not find the empty label we just created at {ts[0]}-{ts[1]} in track {LabelTrack}, "
-                         + "trying at ts=0\n")
+        dprint(f".Could not find the empty label we just created at {ts[0]}-{ts[1]} in track {LabelTrack}, "
+                         + "trying at ts=0")
         setts = True
         labelnum = 0
         for lt in aud_labels:
@@ -144,10 +144,10 @@ def setlabel(LabelTrack,ts,label,endts=None):
                     break
                 labelnum += 1
         if not newlabelnum:
-            sys.stderr.write(f"Could not find the empty label we just created in track {LabelTrack} "
+            dprint(f".Could not find the empty label we just created in track {LabelTrack} "
                              + f"at ts=0 or ts={ts[0]} but we found these blank labels {justblank}\n")
             if len(justblank) == 1:
-                print(f"Using only blank, label# {justblank[0]}")
+                dprint(f".Using only blank, label# {justblank[0]}")
                 newlabelnum = justblank[0]
             elif len(justblank) == 0:
                 sys.exit(f"Could not find any blank labels.")
@@ -215,8 +215,77 @@ def parse_metadatafile(filename):
     return(returnlist)
 
 
+# slightly following the example of vinyl2digital, use the labels to select ranges in audio, then use Export2 to export them
+def export_by_label(audio_track, metadata, interactive=False):
+    global aud_labels
+
+    cwd = os.getcwd()
+    print(f"Storing files in {cwd}")
+
+    # get latest labels
+    aud_labels = pa.get_info(info_type='Labels', format="JSONlist")
+
+    labelnum = 0
+    for lt in aud_labels:
+        for l in lt[1]:
+            if metadata[labelnum]['track'] not in l[2]:
+                sys.exit(f"mismatch between label {l} and metadata {metadata[labelnum]}?")
+            filename = f"{labelnum+1:02d} - {metadata[labelnum]['artist']} - {metadata[labelnum]['track']}.wav"
+            # remove samples annotations, bad-for-unix chars, and lead/trail whitespace
+            filename = re.sub(r'\(\d+:\d+ \+ \d+ samples\)-?', '', filename)
+
+            print(f"--- Exporting #{labelnum} [{l[2]}] as {filename}")
+            pa.do(f"Select: Track={audio_track} Start={l[0]} End={l[1]}")
+            fqfilename = cwd + "/" + filename
+            pa.export(fqfilename, num_channels=2)
+
+            # we're here, invoke wavpack
+            print(f"--- Compacting {filename} with wavpack")
+            try:
+                subprocess.run(["wavpack", fqfilename])
+            except FileNotFoundError:
+                print('Command not found')
+            except subprocess.CalledProcessError as e:
+                print('Error occurred:', e)
+
+            # ok, that went well, now wvtag
+            wvfilename = re.sub(r'.wav$', '.wv', filename)
+            print(f"--- Adding tags to {wvfilename} with wvtag")
+            wvtagcall = ["wvtag",
+                "-w",       "TITLE="+metadata[labelnum]['track'],
+                "-w",      "ARTIST="+metadata[labelnum]['artist'],
+                "-w",       "ALBUM="+metadata[labelnum]['album'],
+                "-w",        "DATE="+metadata[labelnum]['year'],
+                "-w", "TRACKNUMBER="+metadata[labelnum]['num'],
+                "-w",       "GENRE="+metadata[labelnum]['genre'],
+                wvfilename]
+            try:
+                subprocess.run(wvtagcall)
+            except FileNotFoundError:
+                print('Command not found')
+            except subprocess.CalledProcessError as e:
+                print('Error occurred:', e)
+
+            # interactive check if desired
+            if interactive:
+                print("Check result. [yN]", end='')
+                choice = input().lower()
+                if not 'y' in choice:
+                    sys.ext("Exiting")
+
+            # remove wav file
+            print(f"--- Removing {filename}")
+            try:
+                os.remove(filename)
+            except Exception as e:
+                sys.exit("Unable to remove {filename}: {e}")
+
+            # increment label number
+            labelnum += 1
+
+
 def main(args):
-    global audacitytest, debug, aud_clips, aud_tracks
+    global audacitytest, debug, debug_dump, aud_clips, aud_tracks, aud_labels
 
     # Read arguments
     metadatafile = args.metadata
@@ -285,31 +354,42 @@ def main(args):
         else:
             dprint(f".label track found at entry {audio_track}")
 
-    # Create label for each entry in metadata
-    start = 0
-    for track in metadata:
-        if (start > audio_end):
-            print(f"Track {track['track']} starts after end of recorded audio, skipping it and all remaining.")
-            break
-            
-        try:
-            duration = float(track['duration'])
-        except:
-            sys.exit(f"Issue with setting duration to float on entry {track}")
+    # Create label for each entry in metadata - if necessary
+    total_labels = sum(len(track_info[1]) for track_info in aud_labels)
+    create_labels = True
 
-        end = start + duration
-        print(f"Label: '{track['track']:40s}' len: {duration:7.3f} start: {start:7.3f} end: {end:7.3f}")
-        setlabel(label_track,start,track['track'],endts=end)
+    if total_labels > 0:
+        print(f"There are already {total_labels} labels created.  Add {len(metadata)} labels from {metadatafile}? [yN]", end='')
+        choice = input().lower()
+        if not 'y' in choice:
+            create_labels = False
 
-        start = end
+    if create_labels:
+        start = 0
+        for track in metadata:
+            if (start > audio_end):
+                print(f"Track '{track['track']}' starts after end of recorded audio, skipping it and all remaining.")
+                break
+            try:
+                duration = float(track['duration'])
+            except:
+                sys.exit(f"Issue with setting duration to float on entry {track}")
+
+            end = start + duration
+            printt = "'" + track['track'] + "'"
+            print(f"Label: {printt:40s} len: {duration:7.3f} start: {start:7.3f} end: {end:7.3f}")
+            setlabel(label_track,start,track['track'],endts=end)
+
+            start = end
 
     # Verify everything looks ok with user
+    print("Please check the labels in Audacity, adjust if necessary,\nand enter y to continue. [yN]", end='')
+    choice = input().lower()
+    if not 'y' in choice:
+        sys.exit("No 'y' in input, exiting.")
 
-    # Export wav files
-
-    # Invoke wavepack to pack exported files
-
-    # Label wavepack files
+    # Export wav files, pack them with wavpack, label wavpack file, remove wav file
+    export_by_label(audio_track, metadata, interactive=args.interactive)
 
     print("Work complete.")
     exit()
@@ -330,9 +410,10 @@ if __name__ == "__main__":
     """
     parser = argparse.ArgumentParser(description=description, epilog=epilog)
     parser.add_argument('metadata', help='Metadata file')
-    parser.add_argument('--debug', action='store_true', help='Enable debug text')
+    parser.add_argument('-d', '--debug', action='store_true', help='Enable debug text')
     parser.add_argument('--dump', action='store_true', help='Dump data structures as they are read/created')
     parser.add_argument('--audacitytest', action='store_true', help='Print Audacity commands, but do not send them')
+    parser.add_argument('-i', '--interactive', action='store_true', help='Pause after creating each wavpack file')
 
     args = parser.parse_args()
     main(args)
